77181cc7b53269a76e99e2a80970ff3b8bf39960
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-24 21:18:04 -0300

    Projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+
+import api from './services/api'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("")
+  const [cep, setCep] = useState({})
+
+  const handleSearch = async () => {
+    if (input === "") {
+      alert("Preencha o campo para continuar!")
+      return
+    }
+
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput("")
+    } catch {
+      alert("Erro ao buscar CEP")
+      setInput("")
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input 
+          type="text" 
+          name="cep" 
+          id="cep" 
+          placeholder="Digite seu CEP" 
+          autoComplete="off" 
+          value={input} 
+          onChange={(e) => setInput(e.target.value)} 
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#fff" />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>{cep.cep}</h2>
+  
+          <ul>
+            <li><span>{cep.logradouro}</span></li>
+            <li><span>{cep.complemento}</span></li>
+            <li><span>{cep.bairro}</span></li>
+            <li><span>{cep.estado}</span></li>
+          </ul>
+        </main>
+      )}
+    </div>
   )
 }
 
